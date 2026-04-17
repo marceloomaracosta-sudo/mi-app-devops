69d53b7c6292f29c27c57600044ddabea05233a5
unknown <marceloomaracosta@gmail.com>
2026-04-17 18:15:12 -0300

    update message

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola Marcelo, tu app DevOps funciona 🚀"
+    return "Hola Marcelo, tu app DevOps funciona petoncito🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
